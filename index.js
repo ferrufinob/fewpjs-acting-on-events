3ea4d662c436261b79dfd191f087df6a3947d065
Brenda Ferrufino <ferrufinob@gmail.com>
2021-02-23 22:52:38 -0500

    commit tree 6419e4a1ae1c096ab025bfbb842b407c5b84d446

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Your code here
+// Move Left
+let dodger = document.getElementById("dodger");
+
+function moveDodgerLeft() {
+  let leftNumbers = dodger.style.left.replace("px", "");
+  let left = parseInt(leftNumbers, 10);
+
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`;
+  }
+}
+
+// Move Right
+function moveDodgerRight() {
+  let leftNumbers = dodger.style.left.replace("px", "");
+  let left = parseInt(leftNumbers, 10);
+
+  if (left < 360) {
+    dodger.style.left = `${left + 1}px`;
+  }
+}
+
+// Event Handler
+document.addEventListener("keydown", function (e) {
+  if (e.key === "ArrowLeft") {
+    moveDodgerLeft();
+  } else if (e.key === "ArrowRight") {
+    moveDodgerRight();
+  }
+});
